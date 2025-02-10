57f202ce1b61e5868bd91110af740a624c798110
pinkyquantique <pinky@quantique.ai>
2025-02-10 11:43:15 +0530

    changet for across assist

---
+++ Piaggio-API/JWT/IJwtAuth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcrossAssist_API.JWT
+{
+    public interface IJwtAuth
+    {
+        string AuthenticationToken(string username, string role, Core.Module.CommonList list);
+    }
+}
+ 
